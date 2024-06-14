b044f436cbc6677176dbeb4ed9da5a9c5dcf206e
Majrul <Majrul@MJ-LAPTOP>
2024-06-14 20:23:33 +0530

    updates

--- JavaTraining/src/com/ey/training/oop/interfaces/CarRacingGame.java
+++ JavaTraining/src/com/ey/training/oop/interfaces/CarRacingGame.java
@@ -8,11 +8,6 @@ public class CarRacingGame implements MobileApp {
 	}
 
 	@Override
-	public void pause() {
-		System.out.println("CarRacingGame pause..");		
-	}
-
-	@Override
 	public void stop() {
 		System.out.println("CarRacingGame stop..");
 	}
--- JavaTraining/src/com/ey/training/oop/interfaces/MobileApp.java
+++ JavaTraining/src/com/ey/training/oop/interfaces/MobileApp.java
@@ -1,8 +1,21 @@
 package com.ey.training.oop.interfaces;
 
-public interface MobileApp {
+/*public interface MobileApp {
 
 	void start();
 	void pause();
 	void stop();
+}*/
+
+//Suppose Google wants to make pause method
+//optional for developers to implement
+//we can use Java 8's default method in the interface
+public interface MobileApp {
+
+	void start();
+	void stop();
+
+	default void pause() {
+		
+	}
 }
